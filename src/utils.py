83b744ddc10f08a78a6a0181ca3c6bbe94ea11e9
Rohit Solanki <rsolanki1822@gmail.com>
2023-04-07 23:15:03 +0530

    Prediction Pipeline is completed

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ from src.exception import CustomException
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 import dill
+import pickle
 
 
 def save_object(file_path, obj):
@@ -35,10 +36,18 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
 
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
-            train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
             report[list(models.keys())[i]] = test_model_score
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
